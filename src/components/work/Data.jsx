5b6f97fe345a86758047a6748802418f46beea0e
EkinKar <ekin1260@hotmail.com>
2023-05-24 16:35:56 +0200

    added trading bot

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -1,10 +1,19 @@
 import Work1 from "../../assets/work1.png";
 import Work2 from "../../assets/work2.png";
 import Work3 from "../../assets/work3.png";
+import Work4 from "../../assets/work4.png";
 
 export const projectsData = [
   {
     id: 1,
+    image: Work4,
+    title: "Eko Trading Bot",
+    category: "console base",
+    demo: "https://github.com/EkinKar/binanceTradeBot",
+    code: "https://github.com/EkinKar/binanceTradeBot",
+  },
+  {
+    id: 2,
     image: Work1,
     title: "Yelp Camp",
     category: "full stack",
@@ -12,7 +21,7 @@ export const projectsData = [
     code: "https://github.com/EkinKar/yelpCamp",
   },
   {
-    id: 2,
+    id: 3,
     image: Work2,
     title: "EkoBooking",
     category: "frontend",
@@ -20,7 +29,7 @@ export const projectsData = [
     code: "https://github.com/EkinKar/ekoBooking",
   },
   {
-    id: 3,
+    id: 4,
     image: Work3,
     title: "Shop Admin",
     category: "frontend",
@@ -33,4 +42,5 @@ export const projectsNav = [
   { name: "All" },
   { name: "Full Stack" },
   { name: "Frontend" },
+  { name: "Console Base" },
 ];
